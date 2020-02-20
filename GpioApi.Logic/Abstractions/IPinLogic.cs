f6896503681e4f09add41bc35d8a5cb1f73186f1
Joe Wait <joe.wait@gilmond.com>
2020-02-20 21:25:05 +0000

    Added basic pin commands to logic project

---
+++ GpioApi.Logic/Abstractions/IPinLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Gpio;
+using System.Text;
+
+namespace GpioApi.Logic
+{
+    public interface IPinLogic
+    {
+        void OpenPin(int pinNumber);
+
+        void ClosePin(int pinNumber);
+
+        void SetPinMode(int pinNumber, PinMode pinMode);
+    }
+}
---
+++ GpioApi.Logic/PinLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Gpio;
+using System.Text;
+
+namespace GpioApi.Logic
+{
+    public class PinLogic : IPinLogic
+    {
+        public GpioController controller = new GpioController(PinNumberingScheme.Logical);
+
+        public void OpenPin(int pinNumber) => controller.OpenPin(pinNumber, PinMode.Output);
+
+        public void ClosePin(int pinNumber) => controller.ClosePin(pinNumber);
+
+        public void SetPinMode(int pinNumber, PinMode pinMode) => controller.SetPinMode(pinNumber, pinMode);
+    }
+}
